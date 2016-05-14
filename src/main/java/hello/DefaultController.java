178578443d947c59c87d184a78eb9cfc86c0f3e1
Pranav Sundriyal <psundriyal@expedia.com>
2016-05-14 15:57:58 -0500

    website

--- src/main/java/hello/DefaultController.java
+++ src/main/java/hello/DefaultController.java
@@ -1,17 +1,17 @@
 package hello;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DefaultController {
 
-    @RequestMapping("/")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "index";
+    @RequestMapping("/a")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    //    model.addAttribute("name", name);
+      //  return "index";
+        return "name";
     }
 
 }
